f7faf55ade922481a4be70298b1e9fad1ba5628e
Runninginsilence1 <runninginsilence0@gmail.com>
2023-12-06 22:50:37 +0800

    add: cobra complete

--- cmd/server.go
+++ cmd/server.go
@@ -2,18 +2,16 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/duke-git/lancet/v2/fileutil"
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
 	"github.com/olahol/melody"
 	"github.com/spf13/cobra"
-	"gowscat/common"
-	"gowscat/utils"
 	"log"
-	"net/http"
 	"time"
 )
 
 var serverPort int
+var senderFilePath string
 
 var serverCmd = &cobra.Command{
 	Use:     "server",
@@ -31,76 +29,32 @@ var serverCmd = &cobra.Command{
 
 func init() {
 	serverCmd.Flags().IntVarP(&serverPort, "port", "p", 8080, "server port")
+	serverCmd.Flags().StringVarP(&senderFilePath, "msg", "m", "message.json", "websocket broadcast message path")
 }
 
 // 通过读取配置文件的方式，来做成类似于 postman websocket调试的那种效果；
 func GinServer(port string) {
 	r := gin.Default()
-
 	// 普通接口
-
 	m := NewMelody()
-	_ = m
-
-	// ws服务： 读取配置文件来进行send: send.json里面放一个测试用的数据；
-	upgrade := websocket.Upgrader{CheckOrigin: func(r *http.Request) bool {
-		return true
-	}}
-
-	r.GET("/ws", func(c *gin.Context) {
-		wsConn, err := upgrade.Upgrade(c.Writer, c.Request, nil)
-		if err != nil {
-			log.Printf("WebSocket升级失败：%v", err)
-			c.JSON(401, common.R{
-				Code:    401,
-				Message: "无法启动webSocket协议。请更换最新版本Chrome浏览器。",
-				Data:    nil,
-			})
-			return
-		}
-
-		// 开一个go协程，用来处理发送数据；
-		sendExit := make(chan struct{})
-		fmt.Printf("敲击Enter发送数据:\n")
-		go func() {
-			var payload = &common.R{}
-			var input string
-			for {
-				// 从控制台读取信息，判断是
-				select {
-				case <-sendExit:
-					log.Printf("协程已退出\n")
-					return
-				default:
-					_, err = fmt.Scan(&input)
-					if err != nil {
-						continue
-					}
-					utils.ReadSendMsg(payload)
-					err = wsConn.WriteJSON(payload)
-					if err != nil {
-						log.Println(err)
-						return
-					}
-				}
-			}
-		}()
-
-		// 这里接收到后开始写
-		var msgBytes []byte
+	go func() {
+		var err error
+		_ = err
+		var input string
 		for {
-			_, msgBytes, err = wsConn.ReadMessage()
-			if err != nil {
-				fmt.Printf("[Error] func (conn *WebSocket)ReadJson!\nErr: %v\n", err)
-				sendExit <- struct{}{}
-				return
+			_, _ = fmt.Scan(&input)
+			if input == "send" {
+				BroadcastMsg(m, senderFilePath)
 			}
-			fmt.Println("打印接收到的数据：")
-			fmt.Println(string(msgBytes))
+		}
+	}()
+	r.GET("/ws", func(c *gin.Context) {
+		err := m.HandleRequest(c.Writer, c.Request)
+		if err != nil {
+			log.Fatalln("melody handler request error:", err)
 		}
 	})
-	//r.Run(global.ServerPort)
-	r.Run(port)
+	_ = r.Run(port)
 }
 
 func NewMelody() *melody.Melody {
@@ -120,9 +74,13 @@ func NewMelody() *melody.Melody {
 	m.HandleConnect(func(s *melody.Session) {
 	})
 
-	// 是显示呢还是覆盖到文件中呢？
+	// 直接显示内容
 	m.HandleMessage(func(s *melody.Session, rawBytes []byte) {
+		addr := s.RemoteAddr()
+		msg := string(rawBytes)
 
+		fmt.Printf(`%v says:
+%v`, addr, msg)
 	})
 
 	m.HandleDisconnect(func(s *melody.Session) {
@@ -130,19 +88,14 @@ func NewMelody() *melody.Melody {
 	})
 
 	m.HandleClose(func(session *melody.Session, i int, s string) error {
-		fmt.Println("hook close")
-		fmt.Println("string:", s)
-		fmt.Println("int:", i)
 		return nil
 	})
 
 	return m
 }
 
-func ShowMsg() {
-	addr := "localhost"
-	msg := ""
-
-	fmt.Printf(`%v:
-%v`, addr, msg)
+// 向websocket中发送信息
+func BroadcastMsg(m *melody.Melody, filePath string) {
+	content, _ := fileutil.ReadFileToString(filePath)
+	_ = m.Broadcast([]byte(content))
 }
--- go.mod
+++ go.mod
@@ -3,18 +3,25 @@ module gowscat
 go 1.20
 
 require (
+	github.com/gin-gonic/gin v1.9.1
+	github.com/gorilla/websocket v1.5.0
+	github.com/olahol/melody v1.1.4
+	github.com/spf13/cobra v1.8.0
+	github.com/spf13/viper v1.16.0
+)
+
+require (
 	github.com/bytedance/sonic v1.10.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.0 // indirect
+	github.com/duke-git/lancet/v2 v2.2.7 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.9.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.15.1 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
-	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -25,14 +32,11 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/olahol/melody v1.1.4 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.9 // indirect
 	github.com/spf13/afero v1.9.5 // indirect
 	github.com/spf13/cast v1.5.1 // indirect
-	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.16.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
